02f0566d348fc0f0c9c5c761987f24111fa798ee
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:53:57 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "network_manager_routing_rule_collections" {
+  description = "All network_manager_routing_rule_collection resources"
+  value       = azurerm_network_manager_routing_rule_collection.network_manager_routing_rule_collections
+}
+
